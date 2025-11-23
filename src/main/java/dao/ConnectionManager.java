c65166f5e4ef127f1fe7fa8f83b82b0b95ee732b
MariaBuzarhon <maria.buzarhon@educa.madrid.org>
2025-11-23 13:35:46 +0100

    Cambio de contraseña root

--- src/main/java/dao/ConnectionManager.java
+++ src/main/java/dao/ConnectionManager.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConnectionManager {
     private static final String URL = "jdbc:mysql://localhost:3306/biblioteca?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "3112004";
+    private static final String PASSWORD = "root";
 
 
     static {
