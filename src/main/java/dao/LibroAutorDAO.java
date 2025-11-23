b49d549d3f20290d2bc2c8d24fc4523db3fac39f
María Buzarhon Navarro <maria.buzarhon@educa.madrid.org>
2025-11-23 13:10:16 +0100

    Clase LibroAutor creada

--- src/main/java/dao/LibroAutorDAO.java
+++ src/main/java/dao/LibroAutorDAO.java
@@ -1,4 +1,18 @@
 package dao;
 
+import model.Libro;
+import model.Autor;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface LibroAutorDAO {
+
+    void agregarAutorAlLibro(int libroId, int autorId) throws SQLException;
+
+    void eliminarAutorDelLibro(int libroId, int autorId) throws SQLException;
+
+    List<Autor> obtenerAutoresPorLibro(int libroId) throws SQLException;
+
+    List<Libro> obtenerLibrosPorAutor(int autorId) throws SQLException;
 }
