a8517adfd7b5448e492a680c346966ebcee9f6e5
María Buzarhon Navarro <maria.buzarhon@educa.madrid.org>
2025-11-23 13:09:13 +0100

    Modificación de nombres de los métodos

--- src/main/java/dao/AutorDAO.java
+++ src/main/java/dao/AutorDAO.java
@@ -1,14 +1,13 @@
 package dao;
 
 import model.Autor;
-
 import java.sql.SQLException;
 import java.util.List;
 
 public interface AutorDAO {
-    void insertarAutor(Autor autor) throws SQLException;
-    Autor buscarPorID(int id) throws SQLException;
-    List<Autor> listarAutores() throws SQLException;
-    void actualizarAutor(Autor autor) throws SQLException;
-    void eliminarAutor(int id) throws SQLException;
+    void addAutor(Autor autor) throws SQLException;
+    Autor getAutorById(int id) throws SQLException;
+    List<Autor> getAllAutores() throws SQLException;
+    void updateAutor(Autor autor) throws SQLException;
+    void deleteAutor(int id) throws SQLException;
 }
